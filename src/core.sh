alexreg/plowshare

commit e1e830f918e9ba18a0e9af0839afc14ef634a87a

Redefine all global error codes
plowdown, plowup, plowlist, plowdel and modules are using the same error codes.
It makes the module scripting easier!

core.sh:
- move curl return value into curl() function (was previously in download.sh)
- API functions now return proper error codes:
  > curl()
  > no_arbitrary_wait()
  > wait()
  > retry_limit_not_reached()
  > prompt_for_password
  > detect_perl &  detect_javascript
  > parse_all & parse_line_after_all
  > post_login()

modules:
- can now directly forward return code from core.sh API
  > retry_limit_not_reached || return
  > wait 123 seconds || return
  > no_arbitrary_wait || return
  > post_login ... >/dev/null || return
  > local HTML_PAGE=$(curl ...) || return
  > local LINK_PASSWORD=$(prompt_for_password) || return

- modules return codes must be replaced:
  > return 4 => $ERR_LINK_PASSWORD_REQUIRED
  > return 254 => $ERR_LINK_DEAD
  > return 253 => $ERR_LINK_TEMP_UNAVAILABLE
- I'll replace later:
  > return 1 => $ERR_FATAL

git-svn-id: https://plowshare.googlecode.com/svn/trunk@1585 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

src/core.sh
@@ -21,17 +21,20 @@
 set -o pipefail
 
 # Global error codes
-# On multiple arguments, ERROR_CODE_FATAL_MULTIPLE is returned.
-ERROR_CODE_OK=0
-ERROR_CODE_FATAL=1
-ERROR_CODE_NOMODULE=2
-ERROR_CODE_DEAD_LINK=3
-ERROR_CODE_TEMPORAL_PROBLEM=4
-ERROR_CODE_UNKNOWN_ERROR=5
-ERROR_CODE_TIMEOUT_ERROR=6
-ERROR_CODE_NETWORK_ERROR=7
-ERROR_CODE_PASSWORD_REQUIRED=8
-ERROR_CODE_FATAL_MULTIPLE=101
+# 0 means success or link alive
+ERR_FATAL=1                      # Unexpected result (upstream site updated, etc)
+ERR_NOMODULE=2                   # No module found for processing request
+ERR_NETWORK=3                    # Specific network error (socket reset, curl, etc)
+ERR_LOGIN_FAILED=4               # Correct login/password argument is required
+ERR_MAX_WAIT_REACHED=5           # Refer to plowdown wait timeout (see -t/--timeout command line option)
+ERR_MAX_TRIES_REACHED=6          # Refer to plowdown max tries reached (see --max-retries command line option)
+ERR_CAPTCHA=7                    # Captcha solving failure
+ERR_SYSTEM=8                     # System failure (missing executable, local filesystem, wrong behavior, etc)
+ERR_LINK_TEMP_UNAVAILABLE=10     # Link alive but temporarily unavailable (also refer to plowdown --no-arbitrary-wait command line option)
+ERR_LINK_PASSWORD_REQUIRED=11    # Link alive but requires a password
+ERR_LINK_NEED_PERMISSIONS=12     # Link alive but requires some authentication (premium link)
+ERR_LINK_DEAD=13                 #
+ERR_FATAL_MULTIPLE=100           # 100 + (n) with n = first error code (when multiple arguments)
 
 # Global variables used:
 #   - VERBOSE          Verbose log level (0=none, 1, 2, 3, 4)
@@ -116,7 +119,25 @@ curl() {
         rm -rf "$TEMPCURL"
     fi
 
-    return $DRETVAL
+    case "$DRETVAL" in
+        0)
+           ;;
+        # Partial file / HTTP retrieve error / Operation timeout
+        18 | 22 | 28)
+            log_error "curl retrieve error"
+            return $ERR_NETWORK
+            ;;
+        # Write error
+        23)
+            log_error "write failed, disk full?"
+            return $ERR_SYSTEM
+            ;;
+        *)
+            log_error "curl failed with retcode $DRETVAL"
+            return $ERR_NETWORK
+            ;;
+    esac
+    return 0
 }
 
 curl_with_log() {
@@ -177,7 +198,7 @@ matchi() {
 parse_all() {
     local STRING=$(sed -n "/$1/s/^.*$2.*$/\1/p") &&
         test "$STRING" && echo "$STRING" ||
-        { log_error "parse failed: sed -n \"/$1/$2\""; return 1; }
+        { log_error "parse failed: sed -n \"/$1/$2\""; return $ERR_FATAL; }
 }
 
 # Like parse_all, but get only first match
@@ -203,7 +224,7 @@ parse_quiet() {
 parse_line_after_all() {
     local STRING=$(sed -n "/$1/{n;s/^.*$2.*$/\1/p}") &&
         test "$STRING" && echo "$STRING" ||
-        { log_error "parse failed: sed -n \"/$1/$2\""; return 1; }
+        { log_error "parse failed: sed -n \"/$1/$2\""; return $ERR_FATAL; }
 }
 
 # Like parse_line_after_all, but get only first match
@@ -470,7 +491,7 @@ prompt_for_password() {
     stty echo
 
     echo "$PASSWORD"
-    test -z "$PASSWORD" && return 1 || return 0
+    test -n "$PASSWORD" || return $ERR_LINK_PASSWORD_REQUIRED
 }
 
 # Login and return cookie.
@@ -519,7 +540,7 @@ post_login() {
     # There is no known case of a null $RESULT on successful login.
     if [ -z "$RESULT" -o ! -s "${GLOBAL_COOKIES:-$COOKIE}" ]; then
         log_error "login request failed"
-        return 1
+        return $ERR_LOGIN_FAILED
     fi
 
     log_report "=== COOKIE BEGIN ==="
@@ -558,7 +579,7 @@ javascript() {
 detect_javascript() {
     if ! check_exec 'js'; then
         log_notice "Javascript interpreter not found"
-        return 1
+        return $ERR_SYSTEM
     fi
     type -P 'js'
 }
@@ -570,7 +591,7 @@ detect_javascript() {
 detect_perl() {
     if ! check_exec 'perl'; then
         log_notice "Perl interpreter not found"
-        return 1
+        return $ERR_SYSTEM
     fi
     type -P 'perl'
 }
@@ -593,7 +614,7 @@ wait() {
     fi
     local TOTAL_SECS=$((VALUE * UNIT_SECS))
 
-    timeout_update $TOTAL_SECS || return 1
+    timeout_update $TOTAL_SECS || return
 
     local REMAINING=$TOTAL_SECS
     local MSG="Waiting $VALUE $UNIT_STR..."
@@ -616,14 +637,14 @@ retry_limit_not_reached() {
     test -z "$PS_RETRY_LIMIT" && return
     log_notice "Tries left: $PS_RETRY_LIMIT"
     (( PS_RETRY_LIMIT-- ))
-    test "$PS_RETRY_LIMIT" -ge 0
+    test "$PS_RETRY_LIMIT" -ge 0 || return $ERR_MAX_TRIES_REACHED
 }
 
 # Related to --no-arbitrary-wait plowdown command line option
 no_arbitrary_wait() {
     if test "$NOARBITRARYWAIT"; then
         log_debug "File temporarily unavailable"
-        return $ERROR_CODE_TEMPORAL_PROBLEM
+        return $ERR_LINK_TEMP_UNAVAILABLE
     fi
     log_debug "Arbitrary wait"
     return 0
@@ -650,7 +671,7 @@ ocr() {
     if [ $? -ne 0 ]; then
         rm -f $TIFF $TEXT
         log_error "$LOG"
-        return 1
+        return $ERR_SYSTEM
     fi
 
     cat $TEXT
@@ -857,15 +878,6 @@ debug_options() {
     done <<< "$OPTIONS"
 }
 
-# $1: module name
-# $2: option family name (string, example:UPLOAD)
-get_options_for_module() {
-    MODULE=$1
-    NAME=$2
-    VAR="MODULE_$(echo $MODULE | uppercase)_${NAME}_OPTIONS"
-    echo "${!VAR}"
-}
-
 # Look for a configuration module variable
 # Example: MODULE_ZSHARE_DOWNLOAD_OPTIONS (result can be multiline)
 #
@@ -1004,7 +1016,7 @@ grep_config_modules() {
 
    if [ ! -f "$CONFIG" ]; then
        stderr "can't find config file"
-       return 1
+       return $ERR_SYSTEM
    fi
 
    sed -ne "/^[^#].*|[[:space:]]*$1[[:space:]]*|/p" $CONFIG | \
@@ -1039,6 +1051,16 @@ drop_empty_lines() {
     sed '/^[ 	]*$/d'
 }
 
+# $1: module name
+# $2: option family name (string, example:UPLOAD)
+# stdout: options list (one per line)
+get_options_for_module() {
+    MODULE=$1
+    NAME=$2
+    VAR="MODULE_$(echo $MODULE | uppercase)_${NAME}_OPTIONS"
+    echo "${!VAR}"
+}
+
 # Example: 12345 => "3h25m45s"
 # $1: duration (integer)
 splitseconds() {
@@ -1058,7 +1080,7 @@ timeout_update() {
     log_notice "Time left to timeout: $PS_TIMEOUT secs"
     if [[ "$PS_TIMEOUT" -lt "$WAIT" ]]; then
         log_debug "timeout reached (asked $WAIT secs to wait, but remaining time is $PS_TIMEOUT)"
-        return 1
+        return $ERR_MAX_WAIT_REACHED
     fi
     (( PS_TIMEOUT -= WAIT ))
 }

src/download.sh
@@ -201,32 +201,70 @@ download() {
             echo "$DURL"
             rm -f "$COOKIES"
             break
-        elif test $DRETVAL -eq 253; then
-            log_notice "Warning: file link is alive but not currently available"
+        fi
+
+        case "$DRETVAL" in
+            0)
+                ;;
+            $ERR_LOGIN_FAILED)
+                log_notice "Login process failed. Bad username/password or unepected content"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            $ERR_LINK_TEMP_UNAVAILABLE)
+                log_notice "Warning: file link is alive but not currently available"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            $ERR_LINK_PASSWORD_REQUIRED)
+                log_notice "You must provide a password"
+                mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$DURL" "PASSWORD"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            $ERR_LINK_NEED_PERMISSIONS)
+                log_notice "Insufficient permissions (premium link?)"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            $ERR_LINK_DEAD)
+                log_notice "Warning: file link is not alive"
+                mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$DURL" "NOTFOUND"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            $ERR_MAX_WAIT_REACHED)
+                log_notice "Delay limit reached (${FUNCTION})"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            $ERR_MAX_TRIES_REACHED)
+                log_notice "Retry limit reached (${FUNCTION})"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            $ERR_CAPTCHA)
+                log_notice "Error: decoding captcha (${FUNCTION})"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            $ERR_SYSTEM)
+                log_notice "System failure (${FUNCTION})"
+                rm -f "$COOKIES"
+                return $DRETVAL
+                ;;
+            *)
+                log_error "failed inside ${FUNCTION}() [$DRETVAL]"
+                rm -f "$COOKIES"
+                return $ERR_FATAL
+                ;;
+        esac
+
+        # Sanity check
+        if test -z "$FILE_URL"; then
+            log_error "Output URL expected"
             rm -f "$COOKIES"
-            return $ERROR_CODE_TEMPORAL_PROBLEM
-        elif test $DRETVAL -eq 254; then
-            log_notice "Warning: file link is not alive"
-            mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$DURL" "NOTFOUND"
-            rm -f "$COOKIES"
-            return $ERROR_CODE_DEAD_LINK
-        elif test $DRETVAL -eq 2; then
-            log_error "delay limit reached (${FUNCTION})"
-            rm -f "$COOKIES"
-            return $ERROR_CODE_TIMEOUT_ERROR
-        elif test $DRETVAL -eq 3; then
-            log_error "retry limit reached (${FUNCTION})"
-            rm -f "$COOKIES"
-            return $ERROR_CODE_TIMEOUT_ERROR
-        elif test $DRETVAL -eq 4; then
-            log_error "password required (${FUNCTION})"
-            mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$DURL" "PASSWORD"
-            rm -f "$COOKIES"
-            return $ERROR_CODE_PASSWORD_REQUIRED
-        elif test $DRETVAL -ne 0 -o -z "$FILE_URL"; then
-            log_error "failed inside ${FUNCTION}()"
-            rm -f "$COOKIES"
-            return $ERROR_CODE_UNKNOWN_ERROR
+            return $ERR_FATAL
         fi
 
         log_notice "File URL: $FILE_URL"
@@ -308,27 +346,7 @@ download() {
                 -o "$FILENAME_TMP" "$FILE_URL") || DRETVAL=$?
 
             rm -f "$COOKIES"
-
-            case "$DRETVAL" in
-                0) ;;
-                # Partial file / HTTP retrieve error / Operation timeout
-                18|22|28)
-                    local WAIT=60
-                    log_error "curl failed with retcode $DRETVAL"
-                    log_error "retry after a safety wait ($WAIT seconds)"
-                    sleep $WAIT
-                    continue
-                    ;;
-                # Write error
-                23)
-                    log_error "write failed, disk full?"
-                    return $ERROR_CODE_UNKNOWN_ERROR
-                    ;;
-                *)
-                    log_error "failed downloading $DURL"
-                    return $ERROR_CODE_NETWORK_ERROR
-                    ;;
-            esac
+            test "$DRETVAL" -eq 0 || return $DRETVAL
 
             if [ "$CODE" = 416 ]; then
                 # If module can resume transfer, we assume here that this error
@@ -359,6 +377,7 @@ download() {
         mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$DURL" "" "|$FILENAME_OUT"
         break
     done
+    return 0
 }
 
 #
@@ -369,7 +388,7 @@ download() {
 LIBDIR=$(absolute_path "$0")
 
 source "$LIBDIR/core.sh"
-MODULES=$(grep_config_modules 'download') || exit 1
+MODULES=$(grep_config_modules 'download') || exit $?
 for MODULE in $MODULES; do
     source "$LIBDIR/modules/$MODULE.sh"
 done
@@ -386,9 +405,9 @@ else
     VERBOSE=2
 fi
 
-test "$HELP" && { usage; exit $ERROR_CODE_OK; }
-test "$GETVERSION" && { echo "$VERSION"; exit $ERROR_CODE_OK; }
-test $# -ge 1 || { usage; exit $ERROR_CODE_FATAL; }
+test "$HELP" && { usage; exit 0; }
+test "$GETVERSION" && { echo "$VERSION"; exit 0; }
+test $# -ge 1 || { usage; exit $ERR_FATAL; }
 
 if [ -n "$TEMP_DIR" ]; then
     TEMP_DIR=$(echo "$TEMP_DIR" | sed -e "s/\/$//")
@@ -396,7 +415,7 @@ if [ -n "$TEMP_DIR" ]; then
     mkdir -p "$TEMP_DIR"
     if [ ! -w "$TEMP_DIR" ]; then
         log_error "error: no write permission"
-        exit $ERROR_CODE_FATAL
+        exit $ERR_FATAL
     fi
 fi
 
@@ -406,7 +425,7 @@ if [ -n "$OUTPUT_DIR" ]; then
     mkdir -p "$OUTPUT_DIR"
     if [ ! -w "$OUTPUT_DIR" ]; then
         log_error "error: no write permission"
-        exit $ERROR_CODE_FATAL
+        exit $ERR_FATAL
     fi
 fi
 
@@ -424,7 +443,7 @@ for ITEM in "$@"; do
                 MODULE='module_null'
             else
                 log_error "Skip: no module for URL ($URL)"
-                RETVALS=(${RETVALS[@]} $ERROR_CODE_NOMODULE)
+                RETVALS=(${RETVALS[@]} $ERR_NOMODULE)
                 mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$URL" "NOMODULE"
                 continue
             fi
@@ -441,10 +460,10 @@ for ITEM in "$@"; do
 done
 
 if [ ${#RETVALS[@]} -eq 0 ]; then
-    exit $ERROR_CODE_OK
+    exit 0
 elif [ ${#RETVALS[@]} -eq 1 ]; then
     exit ${RETVALS[0]}
 else
     log_debug "retvals:${RETVALS[@]}"
-    exit $ERROR_CODE_FATAL_MULTIPLE
+    exit $((ERR_FATAL_MULTIPLE + ${RETVALS[0]}))
 fi

src/delete.sh
@@ -73,7 +73,7 @@ usage() {
 LIBDIR=$(absolute_path "$0")
 
 source "$LIBDIR/core.sh"
-MODULES=$(grep_config_modules 'delete') || exit 1
+MODULES=$(grep_config_modules 'delete') || exit $?
 for MODULE in $MODULES; do
     source "$LIBDIR/modules/$MODULE.sh"
 done
@@ -90,9 +90,9 @@ else
     VERBOSE=2
 fi
 
-test "$HELP" && { usage; exit $ERROR_CODE_OK; }
-test "$GETVERSION" && { echo "$VERSION"; exit $ERROR_CODE_OK; }
-test $# -ge 1 || { usage; exit $ERROR_CODE_FATAL; }
+test "$HELP" && { usage; exit 0; }
+test "$GETVERSION" && { echo "$VERSION"; exit 0; }
+test $# -ge 1 || { usage; exit $ERR_FATAL; }
 set_exit_trap
 
 RETVALS=()
@@ -105,7 +105,7 @@ for URL in "$@"; do
     MODULE=$(get_module "$URL" "$MODULES")
     if test -z "$MODULE"; then
         log_error "Skip: no module for URL ($URL)"
-        RETVALS=(${RETVALS[@]} $ERROR_CODE_NOMODULE)
+        RETVALS=(${RETVALS[@]} $ERR_NOMODULE)
         continue
     fi
 
@@ -116,10 +116,10 @@ for URL in "$@"; do
 done
 
 if [ ${#RETVALS[@]} -eq 0 ]; then
-    exit $ERROR_CODE_OK
+    exit 0
 elif [ ${#RETVALS[@]} -eq 1 ]; then
     exit ${RETVALS[0]}
 else
     log_debug "retvals:${RETVALS[@]}"
-    exit $ERROR_CODE_FATAL_MULTIPLE
+    exit $((ERR_FATAL_MULTIPLE + ${RETVALS[0]}))
 fi

src/list.sh
@@ -75,7 +75,7 @@ usage() {
 LIBDIR=$(absolute_path "$0")
 
 source "$LIBDIR/core.sh"
-MODULES=$(grep_config_modules 'list') || exit 1
+MODULES=$(grep_config_modules 'list') || exit $?
 for MODULE in $MODULES; do
     source "$LIBDIR/modules/$MODULE.sh"
 done
@@ -92,9 +92,9 @@ else
     VERBOSE=2
 fi
 
-test "$HELP" && { usage; exit $ERROR_CODE_OK; }
-test "$GETVERSION" && { echo "$VERSION"; exit $ERROR_CODE_OK; }
-test $# -ge 1 || { usage; exit $ERROR_CODE_FATAL; }
+test "$HELP" && { usage; exit 0; }
+test "$GETVERSION" && { echo "$VERSION"; exit 0; }
+test $# -ge 1 || { usage; exit $ERR_FATAL; }
 set_exit_trap
 
 RETVALS=()
@@ -103,7 +103,7 @@ for URL in "$@"; do
 
     if test -z "$MODULE"; then
         log_error "Skip: no module for URL ($URL)"
-        RETVALS=(${RETVALS[@]} $ERROR_CODE_NOMODULE)
+        RETVALS=(${RETVALS[@]} $ERR_NOMODULE)
         continue
     fi
 
@@ -114,10 +114,10 @@ for URL in "$@"; do
 done
 
 if [ ${#RETVALS[@]} -eq 0 ]; then
-    exit $ERROR_CODE_OK
+    exit 0
 elif [ ${#RETVALS[@]} -eq 1 ]; then
     exit ${RETVALS[0]}
 else
     log_debug "retvals:${RETVALS[@]}"
-    exit $ERROR_CODE_FATAL_MULTIPLE
+    exit $((ERR_FATAL_MULTIPLE + ${RETVALS[0]}))
 fi

src/modules/115.sh
@@ -33,14 +33,14 @@ MODULE_115_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=unused
 
     if match 'file-notfound"' "$HTML_PAGE"; then
         log_debug "file not found"
-        return 254
+        return $ERR_LINK_DEAD
     fi
 
     local LINKS=$(echo "$HTML_PAGE" | parse_all_attr 'ds_url' 'href')
 
     if [ -z "$LINKS" ]; then
         log_error "no link found, site updated?"
-        return 254
+        return $ERR_LINK_DEAD
     fi
 
     test "$CHECK_LINK" && return 0

src/modules/1fichier.sh
@@ -42,19 +42,19 @@ EMAIL,,email:,EMAIL,Field for notification email"
     # Arbitrary wait (local variable)
     NO_FREE_SLOT_IDLE=10
 
-    while retry_limit_not_reached || return 3; do
+    while retry_limit_not_reached || return; do
         PAGE=$(curl -c "$COOKIEFILE" "$URL")
 
         if match "Le fichier demandé n'existe pas." "$PAGE"; then
             log_error "File not found."
-            return 254
+            return $ERR_LINK_DEAD
         fi
 
         test "$CHECK_LINK" && return 0
 
         if match "Téléchargements en cours" "$PAGE"; then
-            no_arbitrary_wait || return 253
-            wait $NO_FREE_SLOT_IDLE seconds || return 2
+            no_arbitrary_wait || return
+            wait $NO_FREE_SLOT_IDLE seconds || return
             continue
         fi
         break
@@ -82,7 +82,7 @@ EMAIL,,email:,EMAIL,Field for notification email"
         LOGIN_DATA='mail=$USER&pass=$PASSWORD&submit=Login'
         post_login "$AUTH" "$COOKIES" "$LOGIN_DATA" "https://www.1fichier.com/en/login.pl" >/dev/null || {
             rm -f $COOKIES
-            return 1
+            return $ERR_LOGIN_FAILED
         }
     fi
 

src/modules/2shared.sh
@@ -37,7 +37,7 @@ MODULE_2SHARED_DELETE_OPTIONS=""
     local PAGE=$(curl "$URL") || return 1
 
     if match "file link that you requested is not valid" "$PAGE"; then
-        return 254
+        return $ERR_LINK_DEAD
     fi
 
     FILE_URL=$(echo "$PAGE" | parse 'window.location' "='\([^']*\)") || return 1
@@ -93,7 +93,7 @@ MODULE_2SHARED_DELETE_OPTIONS=""
     if ! match 'Delete File' "$ADMIN_PAGE"; then
         log_error "File not found"
         rm -f $COOKIES
-        return 254
+        return $ERR_LINK_DEAD
     else
         FORM=$(grep_form_by_name "$ADMIN_PAGE" 'theForm') || {
             log_error "can't get delete form, website updated?";

src/modules/4shared.sh
@@ -45,7 +45,7 @@ MODULE_4SHARED_LIST_OPTIONS=""
         return 1
     elif match 'The file link that you requested is not valid.' "$PAGE"; then
         log_error "File not found!"
-        return 254
+        return $ERR_LINK_DEAD
     fi
 
     WAIT_URL=$(echo "$PAGE" | parse_attr "4shared\.com\/get\/" 'href')
@@ -62,7 +62,7 @@ MODULE_4SHARED_LIST_OPTIONS=""
     FILE_REAL_NAME=$(echo "$WAIT_HTML" | parse_quiet '<b class="blue xlargen">' \
                     'n">\([^<]\+\)' | html_to_utf8 | uri_decode)
 
-    wait $((WAIT_TIME)) seconds || return 2
+    wait $((WAIT_TIME)) seconds || return
 
     echo "$FILE_URL"
     test "$FILE_REAL_NAME" && echo "$FILE_REAL_NAME"
@@ -80,7 +80,7 @@ MODULE_4SHARED_LIST_OPTIONS=""
 
     PAGE=$(curl "$URL")
     match 'src="/images/spacer.gif" class="warn"' "$PAGE" &&
-        { log_error "Link not found"; return 254; }
+        { log_error "Link not found"; return $ERR_LINK_DEAD; }
     echo "$PAGE" | parse_all_attr "alt=\"Download '" href ||
         { log_error "Cannot parse links"; return 1; }
 }

commit 5e7371878cb5d21e218815c4a5016aa07f6dcc2d

download.sh: if final curl call returns HTTP 503, loop
Regression introduced in revision e1e830f918e9ba18a0e9af0839afc14ef634a87a
(Wed Jul 13 17:23:42 2011) when all global error codes were redefined.

This HTTP 503 case is known on megaupload. When download quota is reached, the final link seems normal, but once grabbed, it gets redirected.

src/core.sh
@@ -1419,9 +1419,9 @@ splitseconds() {
 timeout_update() {
     local WAIT=$1
     test -z "$PS_TIMEOUT" && return
-    log_notice "Time left to timeout: $PS_TIMEOUT secs"
+    log_debug "time left to timeout: $PS_TIMEOUT secs"
     if [[ "$PS_TIMEOUT" -lt "$WAIT" ]]; then
-        log_debug "timeout reached (asked $WAIT secs to wait, but remaining time is $PS_TIMEOUT)"
+        log_notice "Timeout reached (asked to wait $WAIT seconds, but remaining time is $PS_TIMEOUT)"
         return $ERR_MAX_WAIT_REACHED
     fi
     (( PS_TIMEOUT -= WAIT ))

src/download.sh
@@ -422,9 +422,17 @@ download() {
                     continue
                 fi
                 DRETVAL=$ERR_NETWORK
-            fi
 
-            test "$DRETVAL" -eq 0 || return $DRETVAL
+            elif [ "$DRETVAL" -eq $ERR_NETWORK ]; then
+                if [ "$CODE" = 503 ]; then
+                    log_error "Unexpected HTTP code ${CODE}, retry after a safety wait"
+                    wait 120 seconds || return
+                    continue
+                fi
+
+            elif [ "$DRETVAL" -ne 0 ]; then
+                return $DRETVAL
+            fi
 
             if [ "$CODE" = 416 ]; then
                 # If module can resume transfer, we assume here that this error
@@ -438,8 +446,8 @@ download() {
                     rm -f "$FILENAME_TMP"
                     continue
                 fi
-            elif ! match "20." "$CODE"; then
-                log_error "Unexpected HTTP code $CODE"
+            elif [ "${CODE:0:2}" = 20 ]; then
+                log_error "Unexpected HTTP code ${CODE}, restart download"
                 continue
             fi
 