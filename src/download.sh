alexreg/plowshare

commit 5e7371878cb5d21e218815c4a5016aa07f6dcc2d

download.sh: if final curl call returns HTTP 503, loop
Regression introduced in revision e1e830f918e9ba18a0e9af0839afc14ef634a87a
(Wed Jul 13 17:23:42 2011) when all global error codes were redefined.

This HTTP 503 case is known on megaupload. When download quota is reached, the final link seems normal, but once grabbed, it gets redirected.

src/download.sh
@@ -422,9 +422,17 @@ download() {
                     continue
                 fi
                 DRETVAL=$ERR_NETWORK
-            fi
 
-            test "$DRETVAL" -eq 0 || return $DRETVAL
+            elif [ "$DRETVAL" -eq $ERR_NETWORK ]; then
+                if [ "$CODE" = 503 ]; then
+                    log_error "Unexpected HTTP code ${CODE}, retry after a safety wait"
+                    wait 120 seconds || return
+                    continue
+                fi
+
+            elif [ "$DRETVAL" -ne 0 ]; then
+                return $DRETVAL
+            fi
 
             if [ "$CODE" = 416 ]; then
                 # If module can resume transfer, we assume here that this error
@@ -438,8 +446,8 @@ download() {
                     rm -f "$FILENAME_TMP"
                     continue
                 fi
-            elif ! match "20." "$CODE"; then
-                log_error "Unexpected HTTP code $CODE"
+            elif [ "${CODE:0:2}" = 20 ]; then
+                log_error "Unexpected HTTP code ${CODE}, restart download"
                 continue
             fi
 

src/core.sh
@@ -1419,9 +1419,9 @@ splitseconds() {
 timeout_update() {
     local WAIT=$1
     test -z "$PS_TIMEOUT" && return
-    log_notice "Time left to timeout: $PS_TIMEOUT secs"
+    log_debug "time left to timeout: $PS_TIMEOUT secs"
     if [[ "$PS_TIMEOUT" -lt "$WAIT" ]]; then
-        log_debug "timeout reached (asked $WAIT secs to wait, but remaining time is $PS_TIMEOUT)"
+        log_notice "Timeout reached (asked to wait $WAIT seconds, but remaining time is $PS_TIMEOUT)"
         return $ERR_MAX_WAIT_REACHED
     fi
     (( PS_TIMEOUT -= WAIT ))

commit 29c43e45041a741540924a22c7fb21cbd7e62449

download.sh (download): fix "Unexpected HTTP code 200, restart download" bug
Regression introduced in revision 5e7371878cb5d21e218815c4a5016aa07f6dcc2d.

{code}
elif ! match "20." "$CODE"; then
{code}

was wrongly translated to

{code}
elif [ "${CODE:0:2}" = 20 ]; then
{code}

src/download.sh
@@ -446,7 +446,7 @@ download() {
                     rm -f "$FILENAME_TMP"
                     continue
                 fi
-            elif [ "${CODE:0:2}" = 20 ]; then
+            elif [ "${CODE:0:2}" != 20 ]; then
                 log_error "Unexpected HTTP code ${CODE}, restart download"
                 continue
             fi