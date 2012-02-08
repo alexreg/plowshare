alexreg/plowshare

commit c6176485ce7c85e1c2da5d18ca494629dc952643

modules (download function): drop 'test "$FILENAME"' ending statement
It's commun for modules to finish download function with:

<code>
echo "$FILE_URL"
test "$FILENAME" && echo "$FILENAME"
return 0 # maybe not present..
</code>

As plowdown makes two reads, an empty echo is not a problem, and it simplify module code.

<code>
echo "$FILE_URL"
echo "$FILENAME"
</code>

is just enough. $FILENAME may be empty is not a problem (filename will be guessed from url).

Modified modules (11):
- 1fichier
- 2shared
- 4shared
- bayfiles
- dataport_cz
- euroshare_eu
- mediafire
- megashares
- netload_in
- uploaded_to
- wupload

src/modules/uploaded_to.sh
@@ -172,7 +172,7 @@ uploaded_to_download() {
     fi
 
     echo "$FILE_URL"
-    test "$FILE_NAME" && echo "$FILE_NAME"
+    echo "$FILE_URL"
 }
 
 # Upload a file to uploaded.to

src/download.sh
@@ -220,10 +220,10 @@ download() {
             local TRY=0
 
             while :; do
-                $FUNCTION "$@" "$COOKIES" "$URL_ENCODED" >$DRESULT || DRETVAL=$?
+                $FUNCTION "$@" "$COOKIES" "$URL_ENCODED" >"$DRESULT" || DRETVAL=$?
 
                 if [ $DRETVAL -eq $ERR_LINK_TEMP_UNAVAILABLE ]; then
-                    read AWAIT <$DRESULT
+                    read AWAIT <"$DRESULT"
 
                     # --no-extra-wait option specified
                     test -n "$NOEXTRAWAIT" && break
@@ -260,7 +260,7 @@ download() {
                 DRETVAL=0
             done
 
-            { read FILE_URL; read FILENAME; } <$DRESULT || true
+            { read FILE_URL; read FILENAME; } <"$DRESULT" || true
             rm -f "$DRESULT"
         else
             $FUNCTION "$@" "$COOKIES" "$URL_ENCODED" >/dev/null || DRETVAL=$?

src/modules/1fichier.sh
@@ -69,9 +69,7 @@ MODULE_1FICHIER_UPLOAD_REMOTE_SUPPORT=no
     FILENAME=$(echo "$PAGE" | parse_quiet '<title>' '<title>Téléchargement du fichier : *\([^<]*\)')
 
     echo "$FILE_URL"
-    test "$FILENAME" && echo "$FILENAME"
-
-    return 0
+    echo "$FILENAME"
 }
 
 # Upload a file to 1fichier.tld

src/modules/2shared.sh
@@ -49,7 +49,7 @@ MODULE_2SHARED_DELETE_OPTIONS=""
     FILENAME=$(echo "$PAGE" | parse_tag title | parse . '^\(.*\) 2shared - download$')
 
     echo "$FILE_URL"
-    test "$FILENAME" && echo "$FILENAME"
+    echo "$FILENAME"
 }
 
 # Upload a file to 2shared.com

src/modules/4shared.sh
@@ -65,8 +65,7 @@ MODULE_4SHARED_LIST_OPTIONS=""
     wait $((WAIT_TIME)) seconds || return
 
     echo "$FILE_URL"
-    test "$FILE_NAME" && echo "$FILE_NAME"
-    return 0
+    echo "$FILE_NAME"
 }
 
 # List a 4shared folder URL

src/modules/bayfiles.sh
@@ -117,12 +117,11 @@ bayfiles_download() {
         FILE_URL=$(echo "$PAGE" | parse_attr 'class="highlighted-btn' 'href') || return
     fi
 
-    echo "$FILE_URL"
-
     # Extract filename from $PAGE, work for both cases
     FILENAME=$(echo "$PAGE" | parse_attr_quiet 'title="' 'title')
 
-    test "$FILENAME" && echo "$FILENAME"
+    echo "$FILE_URL"
+    echo "$FILENAME"
 }
 
 # Upload a file to bayfiles

src/modules/dataport_cz.sh
@@ -74,7 +74,7 @@ dataport_cz_download() {
     FILE_URL=$(uri_encode_file "$FILE_URL")
 
     echo "$FILE_URL"
-    test "$FILENAME" && echo "$FILENAME"
+    echo "$FILENAME"
 }
 
 # Upload a file to dataport.cz

src/modules/euroshare_eu.sh
@@ -80,7 +80,5 @@ euroshare_eu_download() {
     fi
 
     echo "$FILE_URL"
-    test "$FILENAME" && echo "$FILENAME"
-
-    return 0
+    echo "$FILENAME"
 }

commit 9ebc8881a183132d151a19d3680f481bc34fbbb2

uploaded.to (download): fix regression introduced in revision c6176485ce7c85e1c2da5d18ca494629dc952643
+ add case "requested file isn't available anymore"

src/modules/uploaded_to.sh
@@ -83,8 +83,9 @@ uploaded_to_download() {
         return $ERR_FATAL
     fi
 
-    # file does not exist
-    if match 'uploaded\.to/404' "$URL"; then
+    # Page not found
+    # The requested file isn't available anymore!
+    if match 'uploaded\.to/\(404\|410\)' "$URL"; then
         return $ERR_LINK_DEAD
     fi
 
@@ -172,7 +173,7 @@ uploaded_to_download() {
     fi
 
     echo "$FILE_URL"
-    echo "$FILE_URL"
+    echo "$FILE_NAME"
 }
 
 # Upload a file to uploaded.to